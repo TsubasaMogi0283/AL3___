fbb624fbcecfb53788a18869158957e0082060a8
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-04-21 00:19:51 +0900

    完成

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -50,6 +50,7 @@ void GameScene::Initialize() {
 	//デバッグカメラの生成
 	debugCamera_ = new DebugCamera(100, 100);
 
+
 	//軸方向表示を有効にする
 	AxisIndicator::GetInstance()->SetVisible(true);
 	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
